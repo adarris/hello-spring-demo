a0ad41b284b148b346487b3105ec8d25b433682e
adriana darris <adrianadarris@gmail.com>
2022-11-15 20:39:00 -0600

    reading class 10. mid 13.4

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,20 +1,22 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloSpringController {
-
+    public String name;
     @RequestMapping(value= "hello", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String hello(RequestParam String name){
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("hello/{name}")
     @ResponseBody
-    public String helloAgain(PathVariable String name){
+    public String helloAgain(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
