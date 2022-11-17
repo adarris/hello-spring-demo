f4acf259e5ef89dc478894e0a7dbfb56c711f8aa
adriana <adrianadarris@gmail.com>
2022-11-17 14:11:18 -0600

    reading class 10. complted 13.5

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloSpringController {
     public String name;
@@ -16,12 +19,24 @@ public class HelloSpringController {
 
     @GetMapping("hello/{name}")
     @ResponseBody
-    public String helloAgain(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloAgain(@PathVariable String name, Model model){
+        String greeting = "what's up, " + name + "?";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm(){
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 }
